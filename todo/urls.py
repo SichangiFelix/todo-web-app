aa76ffb64f9dca477c2d42d6242eddff1452af9b
SichangiFelix <sichangifelix23@gmail.com>
2023-09-22 14:08:48 +0300

    First commit of the todo web application

---
+++ todo/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import TaskList, TaskDetail
+
+urlpatterns = [
+    path('', TaskList.as_view(), name='task'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
+]
