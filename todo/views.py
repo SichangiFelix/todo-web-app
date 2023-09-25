71dfa81c850d82050bead407d54d264ff2013598
SichangiFelix <sichangifelix23@gmail.com>
2023-09-25 12:42:01 +0300

    Implemented registration

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -20,7 +20,14 @@ class TaskList(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['task'] = context['task'].filter(user = self.request.user)
         context['count'] = context['task'].filter(complete = False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['task'] = context['task'].filter(title__icontains = search_input)
+            context['search_input'] = search_input
         return context
+    
+
 
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
@@ -65,6 +72,18 @@ class RegisterPage(FormView):
     form_class  = UserCreationForm
     redirect_authenticated_user = True
     success_url = reverse_lazy('task')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('task')
+        return super(RegisterPage, self).get(*args)
+        
 
 
 
